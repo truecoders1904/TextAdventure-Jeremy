9117e58d1d1dd267d90705d9823e788913f897bc
Jeremy Long <jlong.223.jl@gmail.com>
2019-04-11 11:44:16 -0500

    Made changes to how to get free antibiorics

---
+++ TextAdventure-Jeremy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to publix!");
+            Console.WriteLine("Would you like to know you can get free antibiotics?");
+
+            string response = Console.ReadLine();
+
+            if (response == "Yes")
+            {
+                Console.WriteLine("Do you have a prescription from your physician?");
+                response = Console.ReadLine();
+
+                if (response == "Yes") ;
+                Console.WriteLine("Is it bactrim ds?");
+                Console.ReadLine();
+
+                Console.WriteLine("It's free we will fill your prescription. Come back in 15 mins!");
+            }
+    }
+}
